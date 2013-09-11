c38b8d644e536d9d869a2f8f478926f0ad6e6f10
Tim Caswell <tim@creationix.com>
2013-09-10 19:44:31 -0500

    Reduce the expressions

--- bops.js
+++ bops.js
@@ -1,49 +1,19 @@
 
-var bops = module.exports = {
-  from: from,
-  to: to,
-  is: is,
-  subarray: subarray,
-  join: join,
-  copy: copy,
-  create: create,
-};
-
 var proto = Buffer.prototype;
 var call = Function.prototype.call;
+
+var bops = module.exports = {
+  from: Buffer,
+  to: call.bind(proto.toString),
+  is: Buffer.isBuffer,
+  subarray: call.bind(proto.slice),
+  join: Buffer.concat,
+  copy: call.bind(proto.copy),
+  create: Buffer,
+};
+
 for (var key in proto) {
   if (!(/^(?:write|read)/).test(key)) continue;
   bops[key] = call.bind(proto[key]);
 }
 
-function from(source, encoding) {
-  return new Buffer(source, encoding);
-}
-
-function to(source, encoding) {
-  return source.toString(encoding);
-}
-
-function is(buffer) {
-  return Buffer.isBuffer(buffer);
-}
-
-function subarray(source, from, to) {
-  return arguments.length === 2 ?
-    source.slice(from) :
-    source.slice(from, to);
-}
-
-function join(targets, hint) {
-  return hint !== undefined ?
-    Buffer.concat(targets, hint) :
-    Buffer.concat(targets);
-}
-
-function copy(source, target, targetStart, sourceStart, sourceEnd) {
-  return source.copy(target, targetStart, sourceStart, sourceEnd);
-}
-
-function create(size) {
-  return new Buffer(size);
-}
